41b984d638f72e83f3f456315a7b27a0faa406da
shmad <>
2002-07-31 05:43:24 +0000

    slap me silly

--- dl/cs/cs.h
+++ dl/cs/cs.h
@@ -53,7 +53,7 @@
   #define CONETADMIN_MODE '8'		/* Set to a number we dont use */
   #define NETADMIN_MODE 'N'
   #define TECHADMIN_MODE 'T'
-  #define SERVICESADMIN_MODE 'A'
+  #define SERVICESADMIN_MODE 'a'
   #define NETSERVICE_MODE 'S'
   #define INVISIBLE_MODE 'I'
   #define BOT_MODE 'B'
