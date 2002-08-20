09b3bed311514278e64e9c6486e352920b94e0b9
fishwaldo <>
2002-08-20 09:29:20 +0000

    opsb updates to use adns libary. /msg opsb lookup works now

--- dl/opsb/opsb.c
+++ dl/opsb/opsb.c
@@ -4,7 +4,7 @@
 ** Based from GeoStats 1.1.0 by Johnathan George net@lite.net
 *
 ** NetStats CVS Identification
-** $Id: opsb.c,v 1.3 2002/08/20 04:30:47 fishwaldo Exp $
+** $Id: opsb.c,v 1.4 2002/08/20 09:29:20 fishwaldo Exp $
 */
 
 
@@ -21,15 +21,33 @@ const char opsbversion_time[] = __TIME__;
 char *s_opsb;
 
 
+/* max scans in the max concurrent scans at any one time */
+#define MAX_SCANS 100
+/* max queue is the max amount of scans that may be concurrent and queued. */
+#define MAX_QUEUE MAX_SCANS * 100
 
-struct opsb {
-	int dnscount;
-	int dnsok;
-	char dnsbldomain[125];
-	list_t *dnsqueue;
-} opsb;
 
-adns_query adnsq;
+
+struct scanq {
+	char who[MAXNICK];
+	int state;
+	char lookup[MAXHOST];
+};
+
+typedef struct scanq scaninfo;
+
+/* this is the list of items to be queued */
+list_t *opsbq;
+/* this is the list of currently active scans */
+list_t *opsbl;
+
+
+
+/* these are some status flags */
+#define REPORT_DNS 1
+
+
+void reportdns(char *data, adns_answer *a);
 
 
 Module_Info my_info[] = { {
@@ -51,12 +69,19 @@ Functions my_fn_list[] = {
 };
 
 
+int findscan(const void *key1, const void *key2) {
+        const scaninfo *chan1 = key1;
+        return (strcasecmp(chan1->who, key2));
+}
+
+
+
+
 int __Bot_Message(char *origin, char **argv, int argc)
 {
 	User *u;
-	int i;
-	adns_answer *adnsa;
-	char *show;
+	lnode_t *lnode;
+	scaninfo *scandata;
 
 	u = finduser(origin); 
 	if (!u) { 
@@ -64,23 +89,35 @@ int __Bot_Message(char *origin, char **argv, int argc)
 		return -1; 
 	} 
 	if (!strcasecmp(argv[1], "lookup")) {
-		chanalert(s_opsb, "Looking up %s", argv[2]);
-		i = adns_submit(ads, argv[2], adns_r_a, adns_qf_owner|adns_qf_usevc, NULL, &adnsq);
-		chanalert(s_opsb, "query %d", i);
+		if (argc < 3) {
+			prefmsg(u->nick, s_opsb, "Invalid Syntax. /msg %s help lookup for more help", s_opsb);
+			return 0;
+		}
+		scandata = malloc(sizeof(scaninfo));
+		scandata->state = REPORT_DNS;
+		strncpy(scandata->who, u->nick, MAXNICK);
+		strncpy(scandata->lookup, argv[2], MAXHOST);
+		/* if the lists are full, don't add it, and alert the user */
+		if (list_isfull(opsbl)) {
+			if (list_isfull(opsbq)) {
+				prefmsg(u->nick, s_opsb, "Too Busy. Try again Later");
+				free(scandata);
+				return 0;
+			}
+			prefmsg(u->nick, s_opsb, "OPSB list is full, queuing your request");
+			lnode = lnode_create(scandata);
+			list_append(opsbq, lnode);
+		}
+	
+		if (dns_lookup(scandata->lookup, adns_r_a, reportdns, scandata->who) != 1) {
+			prefmsg(u->nick, s_opsb, "DnsLookup Failed.");
+			free(scandata);
+			return 0;
+		} 
+		lnode = lnode_create(scandata);
+		list_append(opsbl, lnode);
 	}
 	if (!strcasecmp(argv[1], "check")) {
-		i = adns_check(ads, &adnsq, &adnsa, NULL);
-		if (i == EAGAIN) {
-			chanalert(s_opsb, "check again %d", i);
-		} else if (i == ESRCH) {
-			chanalert(s_opsb, "check esrch %d", i );
-		} else {
-			chanalert(s_opsb, "check %d", i);
-		}
-		if (!i) {
-		adns_rr_info(adnsa->type, 0,0,0, adnsa->rrs.bytes, &show);
-		chanalert(s_opsb, "Answer: %s", show);
-		}
 	}
 	return 1;
 }
@@ -118,10 +155,51 @@ EventFnList *__module_get_events() {
 	return my_event_list;
 };
 
+
+void reportdns(char *data, adns_answer *a) {
+	lnode_t *dnslookup;
+	scaninfo *dnsinfo;
+	char *show;
+	int i, len, ri;
+	
+	dnslookup = list_find(opsbl, data, findscan);
+	if (!dnslookup) {
+		log("reportdns(): Ehhh, something wrong here");
+		return;
+	}
+	dnsinfo = lnode_get(dnslookup);
+	if (a) {
+		adns_rr_info(a->type, 0, 0, &len, 0, 0);
+		for(i = 0; i < a->nrrs;  i++) {
+			ri = adns_rr_info(a->type, 0, 0, 0, a->rrs.bytes +i*len, &show);
+			if (!ri) {
+				prefmsg(data, s_opsb, "%s resolves to %s", dnsinfo->lookup, show);
+			} else {
+				prefmsg(data, s_opsb, "DNS error %s", adns_strerror(ri));
+			}
+			free(show);
+		}
+		if (a->nrrs < 1) {
+			prefmsg(data, s_opsb, "%s Does not resolve", dnsinfo->lookup);
+		}
+	} else {
+		prefmsg(data, s_opsb, "A unknown error occured");
+	}	
+	
+	list_delete(opsbl, dnslookup);
+	lnode_destroy(dnslookup);
+	free(dnsinfo);
+}
+
+
+
 void _init() {
 	s_opsb = "opsb";
-	strcpy(opsb.dnsbldomain,"opm.blitzed.org\0");
 	globops(me.name, "OPSB Module Loaded");
+
+	opsbl = list_create(MAX_SCANS);
+	opsbq = list_create(MAX_QUEUE);
+
 }
 
 
