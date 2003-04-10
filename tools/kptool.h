7a0159d72bd73b68df834b4072f05019b744c235
fishwaldo <>
2003-04-10 13:11:29 +0000

    added kptool.h, needed for compiling kptools

---
+++ tools/kptool.h
@@ -0,0 +1,43 @@
+/*
+ * kptool: A tool for viewing and manipulating keeper database
+ *
+ * Copyright (C) 1999-2000 Miklos Szeredi 
+ * Email: mszeredi@inf.bme.hu
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifdef ALLOC_CHECK
+#include <sys/types.h>
+extern void *_iamalloc(size_t size, int id);
+extern void *_iarealloc(void *ptr, size_t size, int id);
+extern void  _iafree(void *ptr, int id);
+#define malloc(size)        _iamalloc(size, 4)
+#define free(ptr)           _iafree(ptr, 4)
+#define realloc(ptr, size)  _iarealloc(ptr, size, 4)
+#endif
+
+
+/* Import, export utils */
+
+int   kp_import (const char *filename, const char *basekey);
+int   kp_export (const char *filename, const char *basekey,
+                 const char *subkey);
+
