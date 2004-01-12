8fa022a8dd87c8dda77940a64abe2f3a93c014b1
Mark <>
2004-01-12 20:13:25 +0000

    fix unreal nicklen

--- Unreal.c
+++ Unreal.c
@@ -204,7 +204,8 @@ sserver_cmd (const char *name, const int numeric, const char *infoline)
 int
 slogin_cmd (const char *name, const int numeric, const char *infoline, const char *pass)
 {
-	sts ("%s TOKEN SJOIN", (me.token ? TOK_PROTOCTL : MSG_PROTOCTL));
+//	sts ("%s TOKEN SJOIN", (me.token ? TOK_PROTOCTL : MSG_PROTOCTL));
+	sts ("%s TOKEN ", (me.token ? TOK_PROTOCTL : MSG_PROTOCTL));
 	sts ("%s %s", (me.token ? TOK_PASS : MSG_PASS), pass);
 	sts ("%s %s %d :%s", (me.token ? TOK_SERVER : MSG_SERVER), name, numeric, infoline);
 	return 1;
