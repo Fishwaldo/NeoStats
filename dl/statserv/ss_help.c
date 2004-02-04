27bd6a3b4cee17606aa34fa475eda3521b3f79c0
Mark <>
2004-02-04 23:41:45 +0000

    fix statserv load problem

--- dl/statserv/ss_help.c
+++ dl/statserv/ss_help.c
@@ -25,7 +25,7 @@
 ** $Id$
 */
 
-#include <stdio.h>
+#include "stats.h"
 
 const char ss_help_about_oneline[]="About StatServ";
 const char ss_help_version_oneline[]="Shows you the current StatServ Version";
