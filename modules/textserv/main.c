24274849be9b812125bdb1cee2ead32be9bcb65b
Mark <>
2005-03-11 20:38:34 +0000

    Remove loveserv and moralserv from build and add framework for textserv

---
+++ modules/textserv/main.c
@@ -0,0 +1,118 @@
+/* NeoStats - IRC Statistical Services 
+** Copyright (c) 1999-2005 Adam Rutter, Justin Hammond, Mark Hetherington
+** http://www.neostats.net/
+**
+**  This program is free software; you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation; either version 2 of the License, or
+**  (at your option) any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with this program; if not, write to the Free Software
+**  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+**  USA
+**
+** NeoStats CVS Identification
+** $Id$
+*/
+
+#include "neostats.h"
+#include "textserv.h"
+
+/** Bot command function prototypes */
+
+/** Bot pointer */
+static Bot *ts_bot;
+
+/** Copyright info */
+const char *ts_copyright[] = {
+	"Copyright (c) 1999-2005, NeoStats",
+	"http://www.neostats.net/",
+	NULL
+};
+
+/** Module info */
+ModuleInfo module_info = {
+	"TextServ",
+	"Network text message service",
+	ts_copyright,
+	ts_about,
+	NEOSTATS_VERSION,
+	CORE_MODULE_VERSION,
+	__DATE__,
+	__TIME__,
+	0,
+	0,
+};
+
+/** Bot comand table */
+static bot_cmd ts_commands[]=
+{
+	{NULL,			NULL,			0, 	0,	NULL, 				NULL}
+};
+
+/** BotInfo */
+static BotInfo ts_botinfo = 
+{
+	"TextServ", 
+	"TextServ1", 
+	"TS", 
+	BOT_COMMON_HOST, 
+	"Network text message service",
+	BOT_FLAG_SERVICEBOT|BOT_FLAG_DEAF, 
+	ts_commands, 
+	NULL,
+};
+
+/** @brief ModInit
+ *
+ *  Init handler
+ *
+ *  @param none
+ *
+ *  @return NS_SUCCESS if suceeds else NS_FAILURE
+ */
+
+int ModInit( void )
+{
+	return NS_SUCCESS;
+}
+
+/** @brief ModSynch
+ *
+ *  Startup handler
+ *  Introduce bot onto network
+ *
+ *  @param none
+ *
+ *  @return NS_SUCCESS if suceeds else NS_FAILURE
+ */
+
+int ModSynch( void )
+{
+	ts_bot = AddBot( &ts_botinfo );
+	if( !ts_bot ) 
+	{
+		return NS_FAILURE;
+	}
+	return NS_SUCCESS;
+}
+
+/** @brief ModFini
+ *
+ *  Fini handler
+ *
+ *  @param none
+ *
+ *  @return NS_SUCCESS if suceeds else NS_FAILURE
+ */
+
+int ModFini (void)
+{
+	return NS_SUCCESS;
+}
