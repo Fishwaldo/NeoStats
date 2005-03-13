fbba57af05dfd775a05c2857b4487ae3028814ce
Mark <>
2005-03-13 21:22:21 +0000

    add support files for plexus and bahamut 1.8

---
+++ src/protocol/bahamut18.h
@@ -0,0 +1,141 @@
+/* NeoStats - IRC Statistical Services 
+** Copyright (c) 1999-2005 Adam Rutter, Justin Hammond, Mark Hetherington
+** http://www.neostats.net/
+**
+**  This program is free software; you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation; either version 2 of the License, or
+**  (at your option) any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with this program; if not, write to the Free Software
+**  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+**  USA
+**
+** NeoStats CVS Identification
+** $Id$
+*/
+#ifndef BAHAMUT18_H
+#define BAHAMUT18_H
+
+/* Messages/Tokens */
+#define MSG_PRIVATE  "PRIVMSG"		/* PRIV */
+#define MSG_WHO      "WHO"	      	/* WHO  -> WHOC */
+#define MSG_WHOIS    "WHOIS"	   	/* WHOI */
+#define MSG_WHOWAS   "WHOWAS"	   	/* WHOW */
+#define MSG_USER     "USER"	   	/* USER */
+#define MSG_NICK     "NICK"	   	/* NICK */
+#define MSG_SERVER   "SERVER"	   	/* SERV */
+#define MSG_LIST     "LIST"	   	/* LIST */
+#define MSG_TOPIC    "TOPIC"	   	/* TOPI */
+#define MSG_INVITE   "INVITE"	   	/* INVI */
+#define MSG_VERSION  "VERSION"		/* VERS */
+#define MSG_QUIT     "QUIT"	   	/* QUIT */
+#define MSG_SQUIT    "SQUIT"	   	/* SQUI */
+#define MSG_KILL     "KILL"	   	/* KILL */
+#define MSG_INFO     "INFO"	   	/* INFO */
+#define MSG_LINKS    "LINKS"	   	/* LINK */
+#define MSG_STATS    "STATS"	   	/* STAT */
+#define MSG_USERS    "USERS"	   	/* USER -> USRS */
+#define MSG_HELP     "HELP"	   	/* HELP */
+#define MSG_ERROR    "ERROR"	   	/* ERRO */
+#define MSG_AWAY     "AWAY"	   	/* AWAY */
+#define MSG_CONNECT  "CONNECT"		/* CONN */
+#define MSG_PING     "PING"	   	/* PING */
+#define MSG_PONG     "PONG"	   	/* PONG */
+#define MSG_OPER     "OPER"	   	/* OPER */
+#define MSG_PASS     "PASS"	   	/* PASS */
+#define MSG_WALLOPS  "WALLOPS"		/* WALL */
+#define MSG_TIME     "TIME"	   	/* TIME */
+#define MSG_NAMES    "NAMES"	   	/* NAME */
+#define MSG_ADMIN    "ADMIN"	   	/* ADMI */
+#define MSG_TRACE    "TRACE"	   	/* TRAC */
+#define MSG_NOTICE   "NOTICE"	   	/* NOTI */
+#define MSG_JOIN     "JOIN"	   	/* JOIN */
+#define MSG_PART     "PART"	   	/* PART */
+#define MSG_LUSERS   "LUSERS"	   	/* LUSE */
+#define MSG_MOTD     "MOTD"	   	/* MOTD */
+#define MSG_MODE     "MODE"	   	/* MODE */
+#define MSG_KICK     "KICK"	   	/* KICK */
+#define MSG_USERHOST "USERHOST"		/* USER -> USRH */
+#define MSG_ISON     "ISON"	   	/* ISON */
+#define MSG_REHASH   "REHASH"	   	/* REHA */
+#define MSG_RESTART  "RESTART"		/* REST */
+#define MSG_CLOSE    "CLOSE"	   	/* CLOS */
+#define MSG_SVINFO   "SVINFO"	   	/* SVINFO */
+#define MSG_SJOIN    "SJOIN"	   	/* SJOIN */
+#define MSG_DIE	     "DIE" 		/* DIE */
+#define MSG_HASH     "HASH"	   	/* HASH */
+#define MSG_DNS      "DNS"   	   	/* DNS  -> DNSS */
+#define MSG_OPERWALL "OPERWALL"		/* OPERWALL */
+#define MSG_GLOBOPS  "GLOBOPS"		/* GLOBOPS */
+#define MSG_CHATOPS  "CHATOPS"		/* CHATOPS */
+#define MSG_GOPER    "GOPER"	   	/* GOPER */
+#define MSG_GNOTICE  "GNOTICE"		/* GNOTICE */
+#define MSG_KLINE    "KLINE"	   	/* KLINE */
+#define MSG_UNKLINE  "UNKLINE"		/* UNKLINE */
+#define MSG_HTM      "HTM"	      	/* HTM */
+#define MSG_SET      "SET"	      	/* SET */
+#define MSG_SAMODE   "SAMODE"    	/* SAMODE */
+#define MSG_CHANSERV "CHANSERV"		/* CHANSERV */
+#define MSG_NICKSERV "NICKSERV"		/* NICKSERV */
+#define MSG_MEMOSERV "MEMOSERV"		/* MEMOSERV */
+#define MSG_ROOTSERV "ROOTSERV"		/* MEMOSERV */
+#define MSG_OPERSERV "OPERSERV"		/* OPERSERV */
+#define MSG_STATSERV "STATSERV" 	/* STATSERV */
+#define MSG_HELPSERV "HELPSERV" 	/* HELPSERV */
+#define MSG_SERVICES "SERVICES"		/* SERVICES */
+#define MSG_IDENTIFY "IDENTIFY"		/* IDENTIFY */
+#define MSG_CAPAB    "CAPAB"	   	/* CAPAB */ 
+#define MSG_LOCOPS   "LOCOPS"	   	/* LOCOPS */
+#define MSG_SVSNICK  "SVSNICK"   	/* SVSNICK */
+#define MSG_SVSNOOP  "SVSNOOP"   	/* SVSNOOP */
+#define MSG_SVSKILL  "SVSKILL"   	/* SVSKILL */
+#define MSG_SVSMODE  "SVSMODE"   	/* SVSMODE */
+#define MSG_AKILL    "AKILL"     	/* AKILL */
+#define MSG_RAKILL   "RAKILL"    	/* RAKILL */
+#define MSG_SILENCE  "SILENCE"   	/* SILENCE */
+#define MSG_WATCH    "WATCH"     	/* WATCH */
+#define MSG_SQLINE   "SQLINE" 		/* SQLINE */
+#define MSG_UNSQLINE "UNSQLINE" 	/* UNSQLINE */
+#define MSG_BURST    "BURST"     	/* BURST */
+#define MSG_DCCALLOW "DCCALLOW"		/* dccallow */
+#define MSG_SGLINE   "SGLINE"           /* sgline */
+#define MSG_UNSGLINE "UNSGLINE"         /* unsgline */
+#define MSG_DKEY     "DKEY"		/* diffie-hellman negotiation */
+#define MSG_NS	     "NS"            	/* NickServ commands */
+#define MSG_CS	     "CS"            	/* ChanServ commands */
+#define MSG_MS	     "MS"            	/* MemoServ commands */
+#define MSG_RS	     "RS"            	/* RootServ commands */
+#define MSG_OS	     "OS"            	/* OperServ commands */
+#define MSG_SS	     "SS"            	/* StatServ commands */
+#define MSG_HS	     "HS"            	/* StatServ commands */
+
+/* Umodes */
+#define UMODE_SERVNOTICE	0x00100000	/* umode +s - Server notices */
+#define UMODE_KILLS			0x00200000	/* umode +k - Server kill messages */
+#define UMODE_FLOOD			0x00400000	/* umode +f - Server flood messages */
+#define UMODE_SPY			0x00800000	/* umode +y - Stats/links */
+#define UMODE_DEBUG			0x00100000	/* umode +d - Debug info */
+#define UMODE_GLOBOPS		0x00200000	/* umode +g - Globops */
+#define UMODE_CHATOPS		0x00400000	/* umode +b - Chatops */
+#define UMODE_ROUTE			0x00800000	/* umode +n - Routing Notices */
+#define UMODE_SPAM			0x01000000	/* umode +m - spambot notices */
+#define UMODE_OPERNOTICE	0x02000000	/* umode +e - oper notices for the above +D */
+#define UMODE_SQUELCH		0x04000000	/* umode +x - Squelch with notice */
+#define UMODE_SQUELCHN		0x08000000	/* umode +X - Squelch without notice */
+#define UMODE_HIDDENDCC     0x10000000	/* umode +D - Hidden dccallow umode */
+#define UMODE_THROTTLE		0x20000000	/* umode +F - no cptr->since message rate throttle */
+#define UMODE_REJ			0x40000000	/* umode +j - client rejection notices */
+#define UMODE_ULINEKILL     0x80000000	/* umode +K - U: lined server kill messages */
+
+/* Cmodes */
+#define CMODE_MODREG	0x02000000
+#define CMODE_LISTED	0x04000000
+
+#endif /* BAHAMUT18_H */
---
+++ src/protocol/plexus.c
@@ -0,0 +1,379 @@
+/* NeoStats - IRC Statistical Services 
+** Copyright (c) 1999-2005 Adam Rutter, Justin Hammond, Mark Hetherington
+** http://www.neostats.net/
+**
+**  This program is free software; you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation; either version 2 of the License, or
+**  (at your option) any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with this program; if not, write to the Free Software
+**  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+**  USA
+**
+** NeoStats CVS Identification
+** $Id$
+*/
+
+#include "plexus.h"
+#include "neostats.h"
+#include "ircd.h"
+#include "services.h"
+
+static void m_server (char *origin, char **argv, int argc, int srv);
+static void m_mode (char *origin, char **argv, int argc, int srv);
+static void m_nick (char *origin, char **argv, int argc, int srv);
+static void m_topic (char *origin, char **argv, int argc, int srv);
+static void m_svinfo (char *origin, char **argv, int argc, int srv);
+static void m_burst (char *origin, char **argv, int argc, int srv);
+static void m_sjoin (char *origin, char **argv, int argc, int srv);
+
+/* buffer sizes */
+const int proto_maxhost		= (128 + 1);
+const int proto_maxpass		= (32 + 1);
+const int proto_maxnick		= (32 + 1);
+const int proto_maxuser		= (10 + 1);
+const int proto_maxrealname	= (50 + 1);
+const int proto_chanlen		= (200 + 1);
+const int proto_topiclen	= (512 + 1);
+
+ProtocolInfo protocol_info = {
+	/* Protocol options required by this IRCd */
+	PROTOCOL_SJOIN,
+	/* Protocol options negotiated at link by this IRCd */
+	PROTOCOL_UNKLN,
+	/* Features supported by this IRCd */
+	0,
+	"+o",
+	"+o",
+};
+
+/* this is the command list and associated functions to run */
+ircd_cmd cmd_list[] = {
+	/* Command Token Function usage */
+	{MSG_PRIVATE, 0, _m_private, 0},
+	{MSG_NOTICE, 0, _m_notice, 0},
+	{MSG_STATS, 0, _m_stats, 0},
+	{MSG_VERSION, 0, _m_version, 0},
+	{MSG_MOTD, 0, _m_motd, 0},
+	{MSG_ADMIN, 0, _m_admin, 0},
+	{MSG_CREDITS, 0, _m_credits, 0},
+	{MSG_SERVER, 0, m_server, 0},
+	{MSG_SQUIT, 0, _m_squit, 0},
+	{MSG_QUIT, 0, _m_quit, 0},
+	{MSG_MODE, 0, m_mode, 0},
+	{MSG_KILL, 0, _m_kill, 0},
+	{MSG_PONG, 0, _m_pong, 0},
+	{MSG_AWAY, 0, _m_away, 0},
+	{MSG_NICK, 0, m_nick, 0},
+	{MSG_TOPIC, 0, m_topic, 0},
+	{MSG_KICK, 0, _m_kick, 0},
+	{MSG_JOIN, 0, _m_join, 0},
+	{MSG_PART, 0, _m_part, 0},
+	{MSG_PING, 0, _m_ping, 0},
+	{MSG_SVINFO, 0, m_svinfo, 0},
+	{MSG_PASS, 0, _m_pass, 0},
+	{MSG_EOB, 0, m_burst, 0},
+	{MSG_SJOIN, 0, m_sjoin, 0},
+	{MSG_CAPAB, 0, _m_capab, 0},
+	{MSG_WALLOPS,	0, _m_wallops, 0},
+	{MSG_CHATOPS,	0, _m_chatops, 0},
+	{0, 0, 0, 0},
+};
+
+mode_init chan_umodes[] = {
+	{'o', CUMODE_CHANOP, 0, '@'},
+	{'v', CUMODE_VOICE, 0, '+'},
+	{'h', CUMODE_HALFOP, 0, '%'},
+	{0, 0, 0},
+};
+
+mode_init chan_modes[] = {
+	{'p', CMODE_PRIVATE, 0},
+	{'s', CMODE_SECRET, 0},
+	{'m', CMODE_MODERATED, 0},
+	{'t', CMODE_TOPICLIMIT, 0},
+	{'i', CMODE_INVITEONLY, 0},
+	{'n', CMODE_NOPRIVMSGS, 0},
+	{'b', CMODE_BAN, MODEPARAM},
+	{'e', CMODE_EXCEPT, MODEPARAM},
+	{'I', CMODE_INVEX, MODEPARAM},
+	{'a', CMODE_HIDEOPS, 0},
+	{'l', CMODE_LIMIT, MODEPARAM},
+	{'k', CMODE_KEY, MODEPARAM},
+	{0, 0, 0},
+};
+
+mode_init user_umodes[] = {
+	{'d', UMODE_DEBUG},
+	{'a', UMODE_ADMIN},
+	{'o', UMODE_OPER},
+	{'l', UMODE_LOCOP},
+	{'b', UMODE_BOTS},
+	{'c', UMODE_CLIENT},
+	{'f', UMODE_FULL},
+	{'g', UMODE_CALLERID},
+	{'i', UMODE_INVISIBLE},
+	{'k', UMODE_SKILL},
+	{'n', UMODE_NCHANGE},
+	{'r', UMODE_REJ},
+	{'s', UMODE_SERVNOTICE},
+	{'u', UMODE_UNAUTH},
+	{'w', UMODE_WALLOP},
+	{'x', UMODE_EXTERNAL},
+	{'y', UMODE_SPY},
+	{'z', UMODE_OPERWALL},
+	{0, 0},
+};
+
+void
+send_eob (const char *server)
+{
+	send_cmd (":%s %s", server, MSG_EOB);
+}
+
+void
+send_server (const char *source, const char *name, const int numeric, const char *infoline)
+{
+	send_cmd (":%s %s %s %d :%s", source, MSG_SERVER, name, numeric, infoline);
+}
+
+void
+send_server_connect (const char *name, const int numeric, const char *infoline, const char *pass, const unsigned long tsboot, const unsigned long tslink)
+{
+	send_cmd ("%s %s :TS", MSG_PASS, pass);
+	send_cmd ("CAPAB :TS EX CHW IE EOB KLN GLN KNOCK HOPS HUB AOPS MX");
+	send_cmd ("%s %s %d :%s", MSG_SERVER, name, numeric, infoline);
+}
+
+void
+send_burst (int b)
+{
+}
+
+void
+send_squit (const char *server, const char *quitmsg)
+{
+	send_cmd ("%s %s :%s", MSG_SQUIT, server, quitmsg);
+}
+
+void 
+send_quit (const char *source, const char *quitmsg)
+{
+	send_cmd (":%s %s :%s", source, MSG_QUIT, quitmsg);
+}
+
+void 
+send_part (const char *source, const char *chan, const char *reason)
+{
+	send_cmd (":%s %s %s :%s", source, MSG_PART, chan, reason);
+}
+
+void 
+send_sjoin (const char *source, const char *target, const char *chan, const unsigned long ts)
+{
+	send_cmd (":%s %s %lu %s + :%s", source, MSG_SJOIN, ts, chan, target);
+}
+
+void 
+send_cmode (const char *source, const char *who, const char *chan, const char *mode, const char *args, const unsigned long ts)
+{
+	send_cmd (":%s %s %s %s %s %lu", who, MSG_MODE, chan, mode, args, ts);
+}
+
+void
+send_nick (const char *nick, const unsigned long ts, const char* newmode, const char *ident, const char *host, const char* server, const char *realname)
+{
+	send_cmd ("%s %s 1 %lu %s %s %s %s :%s", MSG_NICK, nick, ts, newmode, ident, host, server, realname);
+}
+
+void
+send_ping (const char *source, const char *reply, const char *target)
+{
+	send_cmd (":%s %s %s :%s", source, MSG_PING, reply, target);
+}
+
+void 
+send_umode (const char *source, const char *target, const char *mode)
+{
+	send_cmd (":%s %s %s :%s", source, MSG_MODE, target, mode);
+}
+
+void 
+send_numeric (const char *source, const int numeric, const char *target, const char *buf)
+{
+	send_cmd (":%s %d %s :%s", source, numeric, target, buf);
+}
+
+void
+send_pong (const char *reply)
+{
+	send_cmd ("%s %s", MSG_PONG, reply);
+}
+
+void
+send_snetinfo (const char* source, const int prot, const char* cloak, const char* netname, const unsigned long ts)
+{
+	send_cmd (":%s %s 0 %lu %d %s 0 0 0 :%s", source, MSG_SNETINFO, ts, prot, cloak, netname);
+}
+
+void
+send_netinfo (const char* source, const int prot, const char* cloak, const char* netname, const unsigned long ts)
+{
+	send_cmd (":%s %s 0 %lu %d %s 0 0 0 :%s", source, MSG_NETINFO, ts, prot, cloak, netname);
+}
+
+void 
+send_kill (const char *source, const char *target, const char *reason)
+{
+	send_cmd (":%s %s %s :%s", source, MSG_KILL, target, reason);
+}
+
+void 
+send_nickchange (const char *oldnick, const char *newnick, const unsigned long ts)
+{
+	send_cmd (":%s %s %s %lu", oldnick, MSG_NICK, newnick, ts);
+}
+
+void 
+send_kick (const char *source, const char *chan, const char *target, const char *reason)
+{
+	send_cmd (":%s %s %s %s :%s", source, MSG_KICK, chan, target, (reason ? reason : "No Reason Given"));
+}
+
+void 
+send_wallops (const char *source, const char *buf)
+{
+	send_cmd (":%s %s :%s", source, MSG_WALLOPS, buf);
+}
+
+void
+send_invite (const char *source, const char *target, const char *chan) 
+{
+	send_cmd (":%s %s %s %s", source, MSG_INVITE, target, chan);
+}
+
+void
+send_svinfo (const int tscurrent, const int tsmin, const unsigned long tsnow)
+{
+	send_cmd ("%s %d %d 0 :%lu", MSG_SVINFO, tscurrent, tsmin, tsnow);
+}
+
+/* there isn't an akill on Hybrid, so we send a kline to all servers! */
+void 
+send_akill (const char *source, const char *host, const char *ident, const char *setby, const unsigned long length, const char *reason, const unsigned long ts)
+{
+	send_cmd (":%s %s * %lu %s %s :%s", setby, MSG_KLINE, length, ident, host, reason);
+}
+
+void 
+send_rakill (const char *source, const char *host, const char *ident)
+{
+	if (ircd_srv.protocol&PROTOCOL_UNKLN) {
+		send_cmd(":%s %s %s %s", source, MSG_UNKLINE, ident, host);
+	} else {
+		irc_chanalert (ns_botptr, "Please Manually remove KLINES using /unkline on each server");
+	}
+}
+
+void
+send_privmsg (const char *source, const char *target, const char *buf)
+{
+	send_cmd (":%s %s %s :%s", source, MSG_PRIVATE, target, buf);
+}
+
+void
+send_notice (const char *source, const char *target, const char *buf)
+{
+	send_cmd (":%s %s %s :%s", source, MSG_NOTICE, target, buf);
+}
+
+void
+send_globops (const char *source, const char *buf)
+{
+	send_cmd (":%s %s :%s", source, MSG_WALLOPS, buf);
+}
+
+/* source SJOIN unsigned long chan modes param:" */
+static void
+m_sjoin (char *origin, char **argv, int argc, int srv)
+{
+	do_sjoin (argv[0], argv[1], ((argc <= 2) ? argv[1] : argv[2]), argv[4], argv, argc);
+}
+
+static void
+m_burst (char *origin, char **argv, int argc, int srv)
+{
+	send_eob (me.name);
+	do_synch_neostats ();
+}
+
+static void
+m_server (char *origin, char **argv, int argc, int srv)
+{
+	do_server (argv[0], origin, argv[1], NULL, argv[2], srv);
+}
+
+static void
+m_mode (char *origin, char **argv, int argc, int srv)
+{
+	if (argv[0][0] == '#') {
+		do_mode_channel (origin, argv, argc);
+	} else {
+		do_mode_user (argv[0], argv[1]);
+	}
+}
+
+/*  m_nick
+ *    argv[0] = nickname
+ *    argv[1] = hop count
+ *    argv[2] = TS
+ *    argv[3] = umode
+ *    argv[4] = username
+ *    argv[5] = hostname
+ *    argv[6] = server
+ *    argv[7] = ircname
+ */
+static void
+m_nick (char *origin, char **argv, int argc, int srv)
+{
+	if(!srv) {
+		do_nick (argv[0], argv[1], argv[2], argv[4], argv[5], argv[6], 
+			NULL, NULL, argv[3], NULL, argv[7], NULL, NULL);
+	} else {
+		do_nickchange (origin, argv[0], NULL);
+	}
+}
+
+static void
+m_topic (char *origin, char **argv, int argc, int srv)
+{
+	/*
+	** Hybrid uses two different formats for the topic change protocol... 
+	** :user TOPIC channel :topic 
+	** and 
+	** :server TOPIC channel author topicts :topic 
+	** Both forms must be accepted.
+	** - Hwy
+	*/	
+	if (FindUser(origin)) {
+		do_topic (argv[0], origin, NULL, argv[1]);
+	} else if (FindServer(origin)) {
+		do_topic (argv[0], argv[1], argv[2], argv[3]);
+	} else {
+		nlog(LOG_WARNING, "m_topic: can't find topic setter %s for topic %s", origin, argv[1]); 
+	}
+}
+
+static void
+m_svinfo (char *origin, char **argv, int argc, int srv)
+{
+	do_svinfo ();
+}
+
