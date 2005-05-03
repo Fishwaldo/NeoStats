dab98ade5295ff0d4eb01a005d5b92ab069ab695
Mark <>
2005-05-03 21:21:13 +0000

    fix adns_status adns__parse_domain prototype

--- include/adnsinternal.h
+++ include/adnsinternal.h
@@ -592,8 +592,8 @@ typedef enum {
 } parsedomain_flags;
 
 adns_status adns__parse_domain(adns_state ads, int serv, adns_query qu,
-			       vbuf *vb, parsedomain_flags flags,
-			       const byte *dgram, int dglen, int *cbyte_io, int max);
+			       vbuf * vb, adns_queryflags flags,
+			       const byte * dgram, int dglen, int *cbyte_io, int max);
 /* vb must already have been initialised; it will be reset if necessary.
  * If there is truncation, vb->used will be set to 0; otherwise
  * (if there is no error) vb will be null-terminated.
