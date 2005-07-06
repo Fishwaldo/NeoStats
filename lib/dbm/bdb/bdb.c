5de03d4f09d86cce52ef696f7c5fbf2bfefb9143
Fish <>
2005-07-06 11:14:57 +0000

    change source layout to not rebuild protocol and dbm files all the time (pass 1)

---
+++ lib/dbm/bdb/bdb.c
@@ -0,0 +1,151 @@
+/* NeoStats - IRC Statistical Services 
+** Copyright (c) 1999-2005 Adam Rutter, Justin Hammond, Mark Hetherington
+** http://www.neostats.net/
+**
+**  This program is free software; you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation; either version 2 of the License, or
+**  (at your option) any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with this program; if not, write to the Free Software
+**  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+**  USA
+**
+** NeoStats CVS Identification
+** $Id$
+*/
+
+#include "neostats.h"
+#include "nsdbm.h"
+
+/*#ifdef HAVE_DB_H*/
+#if 0
+#include <db.h>
+
+static DBT dbkey;
+static DBT dbdata;
+
+void *DBMOpenTable (const char *name)
+{
+	static char filename[MAXPATH];
+	int dbret;
+	DB *dbp;
+
+	dlog (DEBUG1, "DBMOpenTable");
+	ircsprintf (filename, "%s.bdb", name);
+	if ((dbret = db_create(&dbp, NULL, 0)) != 0) {
+		dlog(DEBUG1, "db_create: %s", db_strerror(dbret));
+		return NULL;
+	}
+#if 0 /* Temp since it does not compile */
+	if ((dbret = dbp->open(dbp, filename, "Data", DB_BTREE, DB_CREATE, 0664)) != 0) {
+		dlog(DEBUG1, "dbp->open: %s", db_strerror(dbret));
+		return NULL;
+	}
+	return (void *)dbp;
+#else
+	return NULL;
+#endif
+}
+
+int DBMCloseTable (void *handle)
+{
+	DB *dbp = (DB *)handle;
+
+	dlog(DEBUG1, "DBACloseTable");
+	dbp->close(dbp, 0); 
+	return NS_SUCCESS;
+}
+
+int DBMGetData (void *handle, char *key, void *data, int size)
+{
+	int dbret;
+	DB *dbp = (DB *)handle;
+
+	dlog(DEBUG1, "DBAFetch %s", key);
+	memset(&dbkey, 0, sizeof(dbkey));
+	memset(&dbdata, 0, sizeof(dbdata));
+	dbkey.data = key;
+	dbkey.size = strlen(key);
+	if ((dbret = dbp->get(dbp, NULL, &dbkey, &dbdata, 0)) == 0)
+	{
+		os_memcpy (data, dbdata.data, size);
+		return NS_SUCCESS;
+	}
+	dlog(DEBUG1, "dbp->get: fail");
+	return NS_FAILURE;
+}
+
+int DBMSetData (void *handle, char *key, void *data, int size)
+{
+	int dbret;
+	DB *dbp = (DB *)handle;
+
+	dlog(DEBUG1, "DBAStore %s %s", key, (char *)data);
+	memset(&dbkey, 0, sizeof(dbkey));
+	memset(&dbdata, 0, sizeof(dbdata));
+	dbkey.data = key;
+	dbkey.size = strlen(key);
+	dbdata.data = data;
+	dbdata.size = size;
+	if ((dbret = dbp->put(dbp, NULL, &dbkey, &dbdata, 0)) != 0) {
+		dlog(DEBUG1, "dbp->put: %s", db_strerror(dbret));
+		return NS_FAILURE;
+	}
+	return NS_SUCCESS;
+}
+
+int DBMGetTableRows (void *handle, DBRowHandler handler)
+{
+	int rowcount = 0;
+
+	/* TODO */
+	return rowcount;
+}
+
+int DBMDelData (void *handle, char * key)
+{
+	/* TODO */
+	return NS_SUCCESS;
+}
+
+#else
+void *DBMOpenTable (const char *name)
+{
+	return NULL;
+}
+
+int DBMCloseTable (void *handle)
+{
+	return NS_FAILURE;
+}
+
+int DBMGetData (void *handle, char *key, void *data, int size)
+{
+	return NS_FAILURE;
+}
+
+int DBMSetData (void *handle, char *key, void *data, int size)
+{
+	return NS_FAILURE;
+}
+
+int DBMGetTableRows (void *handle, DBRowHandler handler)
+{
+	int rowcount = 0;
+
+	/* TODO */
+	return rowcount;
+}
+
+int DBMDelData (void *handle, char * key)
+{
+	return NS_FAILURE;
+}
+#endif
---
+++ lib/dbm/gdbm/gdbm.c
@@ -0,0 +1,137 @@
+/* NeoStats - IRC Statistical Services 
+** Copyright (c) 1999-2005 Adam Rutter, Justin Hammond, Mark Hetherington
+** http://www.neostats.net/
+**
+**  This program is free software; you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation; either version 2 of the License, or
+**  (at your option) any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with this program; if not, write to the Free Software
+**  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+**  USA
+**
+** NeoStats CVS Identification
+** $Id$
+*/
+
+#include "neostats.h"
+#include "gdbmdefs.h"
+#include "gdbmerrno.h"
+#include "extern.h"
+#include "nsdbm.h"
+
+extern const char *gdbm_strerror __P( ( gdbm_error ) );
+
+void *DBMOpenTable( const char *name )
+{
+	static char filename[MAXPATH];
+	gdbm_file_info *gdbm_file;
+	int cache_size = DEFAULT_CACHESIZE;
+
+	dlog( DEBUG4, "DBMOpenTable" );
+	ircsprintf( filename, "%s.gdbm", name );
+	gdbm_file = gdbm_open( filename, 0, GDBM_WRCREAT | GDBM_NOLOCK, 00664, NULL );
+	if( gdbm_file == NULL )
+	{
+		nlog( LOG_ERROR, "gdbm_open fail: %s", gdbm_strerror( gdbm_errno ) );
+		return NULL;
+	}
+	if( gdbm_setopt( gdbm_file, GDBM_CACHESIZE, &cache_size, sizeof( int ) ) == -1 )
+	{
+		nlog( LOG_ERROR, "gdbm_setopt fail: %s", gdbm_strerror( gdbm_errno ) );
+		return NULL;
+	}
+	return ( void * )gdbm_file;
+}
+
+int DBMCloseTable( void *handle )
+{
+	if( !handle ) 
+	{
+		return NS_FAILURE;
+	}
+	gdbm_close( ( gdbm_file_info * )handle ); 
+	return NS_SUCCESS;
+}
+
+int DBMGetData( void *handle, char *key, void *data, int size )
+{
+	datum dbkey;
+	datum dbdata;
+
+	dbkey.dptr = key;
+	dbkey.dsize = strlen( key ) + 1;
+	dbdata = gdbm_fetch( ( gdbm_file_info * )handle, dbkey );
+	if( dbdata.dptr != NULL )
+	{
+		os_memcpy( data, dbdata.dptr, size );
+		free( dbdata.dptr );
+		return NS_SUCCESS;
+	}
+	dlog( DEBUG1, "gdbm_fetch fail: %s %s", key, gdbm_strerror( gdbm_errno ) );
+	return NS_FAILURE;
+}
+
+int DBMSetData( void *handle, char *key, void *data, int size )
+{
+	datum dbkey;
+	datum dbdata;
+
+	dbkey.dptr = key;
+	dbkey.dsize = strlen( key ) + 1;
+	dbdata.dptr = data;
+	dbdata.dsize = size;
+	if( gdbm_store( ( gdbm_file_info * )handle, dbkey, dbdata, GDBM_REPLACE ) != 0 )
+	{
+		dlog( DEBUG1, "gdbm_store fail: %s %s", key, gdbm_strerror( gdbm_errno ) );
+		return NS_FAILURE;
+	}
+	return NS_SUCCESS;
+}
+
+int DBMGetTableRows( void *handle, DBRowHandler handler )
+{
+	datum dbkey;
+	datum dbdata;
+	int rowcount = 0;
+
+	dbkey = gdbm_firstkey( ( gdbm_file_info * )handle );
+	while( dbkey.dptr != NULL )
+	{
+		rowcount++;
+		dlog( DEBUG4, "DBMGetTableRows: key %s", dbkey.dptr );
+		dbdata = gdbm_fetch( ( gdbm_file_info * )handle, dbkey );
+		/* Allow handler to exit the fetch loop */
+		if( handler( dbdata.dptr, dbdata.dsize ) != NS_FALSE  )
+		{
+			free( dbdata.dptr );
+			free( dbkey.dptr );
+			break;
+		}
+		free( dbdata.dptr );
+		dbdata = gdbm_nextkey( ( gdbm_file_info * )handle, dbkey );
+		free( dbkey.dptr );
+		dbkey = dbdata;
+	}
+	return rowcount;
+}
+
+int DBMDelData( void *handle, char *key )
+{
+	datum dbkey;
+
+	dbkey.dptr = key;
+	dbkey.dsize = strlen( key ) + 1;
+	if( gdbm_delete( ( gdbm_file_info * )handle, dbkey ) != 0 )
+	{
+		return NS_FAILURE;
+	}
+	return NS_SUCCESS;
+}
