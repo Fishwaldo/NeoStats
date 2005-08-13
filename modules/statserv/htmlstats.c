8b203dd097a8b3b59d603d6fe12c9eb6b0c9315f
Mark <>
2005-08-13 19:28:28 +0000

    Do **NOT** use + with bitmasks, use \|

--- modules/statserv/htmlstats.c
+++ modules/statserv/htmlstats.c
@@ -483,7 +483,7 @@ int ss_html( void *userptr )
 	os_fclose( opf );
 #ifndef WIN32
     /* update the umode so others can read it and owner can overwrite it */
-    chmod(StatServ.htmlpath, S_IRUSR+S_IWUSR|S_IRGRP|S_IROTH);
+    chmod(StatServ.htmlpath, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
 #endif /* WIN32 */
 
 
