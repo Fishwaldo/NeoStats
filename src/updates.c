27c0938968dc70729241be2a3177191b03d5cb85
Fish <>
2005-10-13 14:58:39 +0000

    as I said, no valid pass as well

--- src/updates.c
+++ src/updates.c
@@ -48,7 +48,7 @@ int32 MQSRecvSock(uint8 * buf, uint32 numBytes, void * arg) {
 int InitUpdate(void) 
 {
 #ifndef WIN32
-	if (mqs.username[0]) {
+	if (mqs.username[0] && mqs.password[0]) {
 		dns_lookup( mqs.hostname,  adns_r_a, GotUpdateAddress, NULL );
 		dlog(DEBUG1, "Updates Initialized successfully");
 	}
