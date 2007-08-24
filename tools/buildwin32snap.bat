91479304808c720266db06fab8d613bd058401aa
Fish <>
2007-08-24 07:31:39 +0000

    make sure the Win32 snapshots got the right filename

--- tools/buildwin32snap.bat
+++ tools/buildwin32snap.bat
@@ -13,4 +13,4 @@ IF NOT EXIST modules mkdir modules
 @copy ..\src\win32\neostats.conf .
 cd ..
 "C:\Program Files\Pantaray\QSetup\Composer.exe" tools\NeoStats.qsp /Compile /Exit
-move tools\NeoStats-Setup.exe tools\NeoStats-Setup-3-0-a3-%1.exe
+move tools\NeoStats-Setup.exe tools\NeoStats-Setup-3-0-a4-%1.exe
