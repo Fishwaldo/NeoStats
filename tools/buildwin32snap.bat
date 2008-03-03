2c8f9be00cc6088fdb33c4315b994eb2874edd10
Fish <>
2008-03-03 10:56:33 +0000

    Fix typo

--- tools/buildwin32snap.bat
+++ tools/buildwin32snap.bat
@@ -2,4 +2,3 @@
 IF EXIST tools\NeoStats-Setup*.exe del tools\NeoStats-Setup*.exe
 "C:\Program Files\Pantaray\QSetup\Composer.exe" tools\NeoStats.qsp /Compile /Exit
 move tools\NeoStats-Setup.exe tools\NeoStats-Setup-3-0-a4-%1.exe
-`
